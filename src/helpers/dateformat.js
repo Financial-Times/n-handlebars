32f0b81d25f8cc04602e70545d7fe7b792483f90
Matthew Andrews <matt@mattandre.ws>
2015-04-15 14:08:31 +0100

    try {} catch {}

--- src/helpers/dateformat.js
+++ src/helpers/dateformat.js
@@ -3,13 +3,18 @@
 var dateFormat = require('dateformat');
 
 module.exports = function(format, options) {
-	if (typeof format !== 'string') {
-		options = format;
-		format = 'isoUtcDateTime';
+	try {
+		if (typeof format !== 'string') {
+			options = format;
+			format = 'isoUtcDateTime';
+		}
+		if (format === 'isoDateTime' || format === 'isoUtcDateTime') {
+			format = 'isoUtcDateTime';
+			return dateFormat(options.fn(this), format, true);
+		}
+		return dateFormat(options.fn(this), format);
+	} catch(err) {
+		console.log(err);
+		return '';
 	}
-	if (format === 'isoDateTime' || format === 'isoUtcDateTime') {
-		format = 'isoUtcDateTime';
-		return dateFormat(options.fn(this), format, true);
-	}
-	return dateFormat(options.fn(this), format);
 };
