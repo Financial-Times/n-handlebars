6a644f61f4ef7fa64d389d0ebcf09e58ac19c8e0
Rhys Evans <rhys.evans@ft.com>
2015-08-13 15:12:50 +0100

    test for standalone handlebasr

---
+++ test/fixtures/view-data.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = {
+	title: "FT",
+	image: "https://avatars0.githubusercontent.com/u/3502508?v=3",
+	date: new Date('Fri Aug 01 2014 00:00:00 GMT'),
+	text : "<p>Paragraph 1</p><p>Paragraph 2</p><p>Paragraph 3</p>",
+	entitiesText: "Something,&nbsp;something,&nbsp;something and something",
+	block1default: 'block1default',
+	block2default: 'block2default',
+	block2override: 'block2override',
+	thing1: 'thing1',
+	thing2: 'thing2',
+	thing3: 'thing3',
+	items: [1,2,3,4,5],
+	obj: {prop: 'val'},
+	partial: 'partial',
+	rootVar: 'iamroot'
+};
