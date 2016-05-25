2fa753a25e62d35fa5ef1c445cd98bcde65808da
Paul Wilson <paul.i.wilson@ft.com>
2016-05-25 15:39:32 +0100

    alow path param to usePartialHelper

--- src/helpers/use-partial.js
+++ src/helpers/use-partial.js
@@ -1,7 +1,11 @@
 'use strict';
-
+var path = require('path');
 var handlebars = require('handlebars');
 
 module.exports = function(name, opts) {
+	if(opts.hash.path){
+		name = path.join(opts.hash.path, name);
+	}
+	
 	return handlebars.partials[name] ? handlebars.partials[name](this, opts) : '';
 };
