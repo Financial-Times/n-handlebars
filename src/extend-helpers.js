82a42d107ae2060382a61a13483cf9660080d608
Ben Fletcher <bjfletcher@gmail.com>
2016-11-01 17:37:36 +0000

    new unlessAll helper (#35)

--- src/extend-helpers.js
+++ src/extend-helpers.js
@@ -9,6 +9,7 @@ module.exports = function (helpers) {
 	helpers.ifAll = require('./helpers/ifAll');
 	helpers.ifSome = require('./helpers/ifSome');
 	helpers.ifBool = require('./helpers/ifBool');
+	helpers.unlessAll = require('./helpers/unlessAll');
 	helpers.unlessEquals = require('./helpers/unlessEquals');
 	helpers.dateformat = require('./helpers/dateformat');
 	helpers.resize = require('./helpers/resize');
