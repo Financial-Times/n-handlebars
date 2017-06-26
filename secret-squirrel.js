15dd3f7445fa6af8e7be55eae68cbac52a45accf
Darren Hurley <ironsidevsquincy@googlemail.com>
2017-06-26 16:37:46 +0100

    Secret squirrel

---
+++ secret-squirrel.js
@@ -0,0 +1,12 @@
+module.exports = {
+	files: {
+		allow: [
+			'test/fixtures/app/bower_components/linked-dep-1'
+		],
+		allowOverrides: []
+	},
+	strings: {
+		deny: [],
+		denyOverrides: []
+	}
+};
