19d1122303395a45dff981734b792122f0110f02
Matt Hinchliffe <i-like-robots@users.noreply.github.com>
2018-04-26 16:03:17 +0100

    Don't allow JSON helper to output root context (#65)
    * Don't allow JSON helper to output root context
    
    * Better JSON error message

--- src/helpers/json.js
+++ src/helpers/json.js
@@ -1,5 +1,9 @@
 'use strict';
 
 module.exports = function (obj) {
+	if (process.env.NODE_ENV !== 'development' && obj.hasOwnProperty('_locals')) {
+		throw Error('For security reasons you may not use the Handlebars JSON helper to output the entire view context');
+	}
+
 	return JSON.stringify(obj);
 };
