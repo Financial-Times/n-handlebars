05a8c39193579df1414542f04c1f1c7f64d1e770
Ekaterina Loshchinina <ekaterina.loshchinina@gmail.com>
2018-04-27 11:16:14 +0100

    The check will not error if undefined is passed (#66)

--- src/helpers/json.js
+++ src/helpers/json.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function (obj) {
-	if (process.env.NODE_ENV !== 'development' && obj.hasOwnProperty('_locals')) {
+	if (process.env.NODE_ENV !== 'development' && Object(obj) === obj && obj.hasOwnProperty('_locals')) {
 		throw Error('For security reasons you may not use the Handlebars JSON helper to output the entire view context');
 	}
 
